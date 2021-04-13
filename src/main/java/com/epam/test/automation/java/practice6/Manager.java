4308b2c5575dd89bc68cc426637bd355c13fd0c2
HvqEMAOU <hvqemaou@email.com>
2021-04-13 18:55:14 +0000

    Update Manager.java

--- src/main/java/com/epam/test/automation/java/practice6/Manager.java
+++ src/main/java/com/epam/test/automation/java/practice6/Manager.java
@@ -1,9 +1,36 @@
 package com.epam.test.automation.java.practice6;
 
-/**
- * <summary>
- * Implement code according to description of task.
- * </summary>
- */
-public class Manager {
+import java.math.BigDecimal;
+
+public class Manager extends Employee {
+	   
+	    private int quantity;
+
+	    public Manager (String name, BigDecimal salary, int clientAmount) {
+	        super (name, salary);
+	        this.quantity = clientAmount;
+	    }
+
+
+	    public BigDecimal getBonus() {
+	        return bonus;
+	    }
+
+	    @Override
+	    public void setBonus(BigDecimal bonus) {
+	    	BigDecimal bd500 = new BigDecimal("500");
+	    	BigDecimal bd1000 = new BigDecimal("1000");   	
+	
+	    	if (quantity>100) {
+	    		BigDecimal bonusOver100 = bd500.add(bonus);
+	    		super.setBonus(bonusOver100);
+	    	}
+	    	else if (quantity>150) {
+	    		BigDecimal bonusOver150 = bd1000.add(bonus);
+	    		super.setBonus(bonusOver150);
+	    	}
+	    	else 
+	    		super.setBonus (bonus);
+	    }
+
 }
