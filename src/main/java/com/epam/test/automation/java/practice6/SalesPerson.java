faa6c5848dbaa3a0aae74b1c888070e5bbbaecd0
HvqEMAOU <hvqemaou@email.com>
2021-04-14 10:20:09 +0000

    Update SalesPerson.java

--- src/main/java/com/epam/test/automation/java/practice6/SalesPerson.java
+++ src/main/java/com/epam/test/automation/java/practice6/SalesPerson.java
@@ -14,6 +14,8 @@ public class SalesPerson extends Employee {
 
     @Override
     public void setBonus(BigDecimal bonus) {
+        if (bonus.compareTo(BigDecimal.ZERO) < 0 || bonus.equals(null))
+	    		throw new IllegalArgumentException();
     	BigDecimal bd2 = new BigDecimal("2");
     	BigDecimal bd3 = new BigDecimal("3");
     	if (percent>100) {
